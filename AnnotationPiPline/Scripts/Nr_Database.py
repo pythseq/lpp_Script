08d59654fffb4cf30c144b9f3cfd2b364d38cbf9
unknown <>
2015-11-13 21:36:26 +0800

    123

--- AnnotationPiPline/Scripts/Nr_Database.py
+++ AnnotationPiPline/Scripts/Nr_Database.py
@@ -6,32 +6,44 @@
   Created: 2015/3/16
 """
 import os,sys
-sys.path.append(os.path.split(__file__)[0]+'/../Lib/')
+from os.path import abspath
+sys.path.append( os.path.split(abspath(__file__))[0]+'/../Lib/' )
 from lpp import *
 from Dependcy import *
-from sqlalchemy import *
-from sqlalchemy.orm import sessionmaker
+usage = "python2.7 %prog [options]"
+parser = OptionParser(usage =usage )
+parser.add_option("-d", "--Database", action="store",
+                  dest="DB_FILE",
+
+                  help="Database File")
 
 
-DB_FILE = open(os.path.abspath(sys.argv[1]),'a')
+parser.add_option("-n", "--Nr", action="store",
+                  dest="NR",
 
-NR_ANNO_DETAIL = open(sys.argv[2],'rU')
+                  help="Nr Ghostz Aligment Result")
+if __name__ == '__main__':
+	(options, args) = parser.parse_args()
 
-data_hash = Ddict()
-for line in NR_ANNO_DETAIL:
-	line_l = line[:-1].split("\t")
-	name = line_l[0]
-	data_hash[name]["Nr_Hit"] = line_l[1]+" "+line_l[2]
-	data_hash[name]["Nr_Eval"] = line_l[-2]
-	data_hash[name]["Nr_Bit_Score"] = line_l[-1]
-	data_hash[name]["Nr_Identity"] = line_l[2]
-	data_hash[name]["Nr_StartQuery"] = line_l[6]
-	data_hash[name]["Nr_EndQuery"] = line_l[7]
-	data_hash[name]["Nr_Mismatch"] = line_l[5]
-	data_hash[name]["Nr_EndSubj"] = line_l[9]
-	data_hash[name]["Nr_StartSubj"] = line_l[8]
-for data in data_hash[name]:
-	data_hash["title"][data] = ""	
-DB_FILE.write(Redis_trans(data_hash))
+	DB_FILE = open( os.path.abspath(options.DB_FILE),'a')
+	
+	NR_ANNO_DETAIL = open(   os.path.abspath(  options.NR ),'rU'   ) 
+	
+	data_hash = Ddict()
+	for line in NR_ANNO_DETAIL:
+		line_l = line[:-1].split("\t")
+		name = line_l[0].split()[0]
+		data_hash[name]["Nr_Hit"] = line_l[1]+" "+line_l[2]
+		data_hash[name]["Nr_Eval"] = line_l[-2]
+		data_hash[name]["Nr_Bit_Score"] = line_l[-1]
+		data_hash[name]["Nr_Identity"] = line_l[2]
+		data_hash[name]["Nr_StartQuery"] = line_l[6]
+		data_hash[name]["Nr_EndQuery"] = line_l[7]
+		data_hash[name]["Nr_Mismatch"] = line_l[5]
+		data_hash[name]["Nr_EndSubj"] = line_l[9]
+		data_hash[name]["Nr_StartSubj"] = line_l[8]
+	for data in data_hash[name]:
+		data_hash["title"][data] = ""	
+	DB_FILE.write(Redis_trans(data_hash))
 
 
--- AnnotationPiPline/Scripts/Nt_Database.py
+++ AnnotationPiPline/Scripts/Nt_Database.py
@@ -6,30 +6,45 @@
   Created: 2015/3/16
 """
 import os,sys
-
-sys.path.append(os.path.split(__file__)[0]+'/../Lib/')
+from os.path import abspath
+sys.path.append( os.path.split(abspath(__file__))[0]+'/../Lib/' )
 from lpp import *
 from Dependcy import *
-DB_FILE = open(os.path.abspath(sys.argv[1]),'a')
-NR_ANNO_DETAIL = open(sys.argv[2],'rU')
-data_hash = Ddict()
-#NR_ANNO_DETAIL.next()
-for line in NR_ANNO_DETAIL:
-	line_l = line[:-1].split("\t")
-	name = line_l[2].split()[0]
-	data_hash[name]["Nt_Hit"] = line_l[5]+" "+line_l[6]
+usage = "python2.7 %prog [options]"
+parser = OptionParser(usage =usage )
+parser.add_option("-d", "--Database", action="store",
+                  dest="DB_FILE",
 
-	data_hash[name]["Nt_Eval"] = line_l[12]
-	data_hash[name]["Nt_Query_From"] = line_l[13]
-	data_hash[name]["Nt_Query_To"] = line_l[14]
-	data_hash[name]["Nt_Hit_From"] = line_l[15]
-	data_hash[name]["Nt_Hit_To"] = line_l[16]
-	data_hash[name]["Nt_BitScore"] = line_l[10]
-	data_hash[name]["Nt_Identity"] = str(int(line_l[-6])*100.0/int(line_l[3]))
+                  help="Database File")
+
+
+parser.add_option("-n", "--Nt", action="store",
+                  dest="NT",
+
+                  help="Nt Ghostz Aligment Result")
+#NR_ANNO_DETAIL.next()
+if __name__ == '__main__':
+	(options, args) = parser.parse_args()
+	DB_FILE = open( os.path.abspath(options.DB_FILE),'a')
 	
+	NR_ANNO_DETAIL = open(   os.path.abspath(  options.NT ),'rU'   ) 	
+	for line in NR_ANNO_DETAIL:
+		line_l = line[:-1].split("\t")
+		name = line_l[2].split()[0]
+		data_hash[name]["Nt_Hit"] = line_l[5]+" "+line_l[6]
 	
-for data in data_hash[name]:
-	data_hash["title"][data] = ""
-	
-DB_FILE.write(Redis_trans(data_hash))
+		data_hash[name]["Nt_Eval"] = line_l[12]
+		data_hash[name]["Nt_Query_From"] = line_l[13]
+		data_hash[name]["Nt_Query_To"] = line_l[14]
+		data_hash[name]["Nt_Hit_From"] = line_l[15]
+		data_hash[name]["Nt_Hit_To"] = line_l[16]
+		data_hash[name]["Nt_BitScore"] = line_l[10]
+		data_hash[name]["Nt_Identity"] = str(int(line_l[-6])*100.0/int(line_l[3]))
+		
+		
+		
+	for data in data_hash[name]:
+		data_hash["title"][data] = ""
+		
+	DB_FILE.write(Redis_trans(data_hash))
 
