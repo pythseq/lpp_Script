1d9792346888531f07845b73a65494fa460732f2
unknown <>
2015-11-14 18:05:42 +0800

    123

--- GO_draw.py
+++ GO_draw.py
@@ -19,7 +19,7 @@ countsTable <- read.delim( exampleFile, header=TRUE, stringsAsFactors=TRUE )
 
 hei<-length(levels(countsTable$term))
 
-pdf("%s.pdf", width = 0.5*hei ,height=hei)
+pdf("%s.pdf", width = 0.5*hei ,height=hei/2)
 bb <-ggplot(countsTable,aes(term,numDEInCat))
 aa<-bb+facet_grid(.~ontology,scales="free_x",space="free")+theme_few()+geom_bar(aes(fill=ontology,position="dodge",order=numDEInCat),stat="identity")+theme(legend.position="none",axis.text.x=element_text(angle=75,hjust=1.0,size=12),axis.title.y = element_text(size = 18),strip.text.x = element_text(size=30,color="darkred",face="bold")  )+ylab("Gene Number")
 aa
