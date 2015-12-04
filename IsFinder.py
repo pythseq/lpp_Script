c8f32faf166881cc35c2b25696210b3e613ec0c0
Your Name <lpp1985@hotmail.com>
2015-12-04 22:10:48 +0800

    123

--- IsFinder.py
+++ IsFinder.py
@@ -6,6 +6,7 @@
   Created: 2015/12/4
 """
 from lpp import *
+import tempfile
 import pandas as pd
 from optparse import OptionParser
 import poster,time,urllib2,urllib
@@ -80,6 +81,10 @@ CAAATCATAGCGATTTGCCGCTTTTT""",
 "msmatch": "-1"	,
 "qssad":	"3"}
 if __name__ == '__main__':
+	(options, args) = parser.parse_args()
+	DATA = fasta_check( open(options.Sequence,'rU') )
+	sequence = re.sub('\s+','',DATA.next()[-1])
+	NUL = open(  options.Nul ,'w' )
 	data = urllib.urlencode(values)
 	req = urllib2.Request(url,data)
 	response = urllib2.urlopen(req)
@@ -87,11 +92,33 @@ if __name__ == '__main__':
 		uploadend = response.read()
 		
 		out_url = re.search("""(https[^']+)""", uploadend).group(1)
+		result = None
+		while not  result:
+			time.sleep(5)
+			end_output = urllib.urlopen(out_url).read()
 		
-		time.sleep(5)
-		end_output = urllib.urlopen(out_url).read()
-		result = re.search("Normal view</a></font><br>(.+)</form>",end_output,re.DOTALL).group(1)
-		print(result)
-		
+			result = re.search("Normal view</a></font><br>(.+)</form>",end_output,re.DOTALL)
+		result = result.group(1)
+		ALN = open( options.Alignment,'w'  )
+		ALN.write( '\t'.join(["Name","Ref_Source","IS_Kind","Function","Ref_Start","Ref_End","Ref_Frame","Seq_Nucl_Length","Seq_Nucleotide","IS_SeqenceIdentity","IS_AlignmentLength","IS_Mismatch","IS_GapLength","IS_QueryStart","IS_QueryEND","IS_RefStart","IS_RefEnd","IS_Evalue","IS_Bitscore"])+'\n' )
+		i=0
+		for line in result.split("\n"):
+			i+=1
+			line_l = line.split("\t")
+			out_data = []
+			isname= line_l[0].split()[0]+"_IS%s"%(i)
+			
+			q_start,q_end = int(line_l[6]),int(line_l[7])
+			if q_start <q_end:
+				frame='+'
+			else:
+				frame='-'
+				q_start,q_end  = q_end,q_start
+			is_seq = sequence[q_start:q_end]
+			NUL.write('>'+isname+' '+line_l[1]+'\n'+is_seq+'\n')
+			is_length = len(is_seq)
+			out_data.append(isname,line_l[0].split()[0],"IS_Element",line_l[1],line_l[6],line_l[7],frame,str(is_length),is_seq)
+			out_data.extend(line_l[3:])
+			ALN.write("\t".join(out_data)+'\n')
 	except:
 		print(end)
