4a6c8b24923dfbcbff830287af27e1abc2546abc
Your Name <lpp1985@hotmail.com>
2015-12-05 14:40:06 +0800

    123

--- diamnond_align.py
+++ diamnond_align.py
@@ -6,8 +6,10 @@
   Created: 2014/11/21
 """
 import sys,shlex,os,subprocess
-
-sys.path.append(os.path.split(__file__)[0]+'/../Lib/')
+from ConfigParser import ConfigParser
+import redis
+import tempfile
+from  termcolor import colored
 from lpp import *
 from optparse import OptionParser
 usage = "python2.7 %prog [options]"
@@ -29,29 +31,48 @@ parser.add_option("-e", "--Evalue", action="store",
                   dest="Evalue",
 
                   help="Evalue !")
+parser.add_option("-n", "--DBName", action="store",
+                  dest="dbname",
+
+                  help="DB Name")
+parser.add_option("-t", "--Type", action="store",
+                  dest="type",
+
+                  help="AlignType, blastx or blastp ,default is blastp",
+                  default ="blastp"
+                  )
+
 if __name__=="__main__":
     (options, args) = parser.parse_args()
     Input = options.Input
     OUTPUT = options.output
     Database = options.Database
     Path = os.path.split(OUTPUT)[0]
+    Type = options.type
+    if Type not in ["blastx","blastp"]:
+        print(colored(  "Type is error! must be blastx or blastp!!!","red"))
+        sys.exit()
     if not Path:
-	Path="./"
+        Path="./"
     if not os.path.exists(Path):
         os.makedirs(Path)
+    Path = Path+'/'
     E_value = options.Evalue
-
+    query_length = {}
     RAW = fasta_check(open(Input,'rU'))
-    #all_seq = {}
-    #for t,s in RAW:
-    #   all_seq[t[1:-1]] = s.strip()+'\n'
-
-    commandline = """    ghostz aln  -i  %s -b 1  -a 64   -d %s   -o /dev/stdout | awk 'NR > 5 {print $0}' |awk -F'[\t]'   '$11<%s'|sed    "s/^finished$//g"   | sed  '/^$/d'   >%s 2>/dev/null"""%( 
+    for t,s in RAW:
+        name = t.split()[0][1:]
+        length = str(len(re.sub("\s+", "", s)))
+        query_length[name]=length
+    temp_name = os.getpid()
+    tmp_file_name = Path+temp_name
+    commandline = """     diamond %s -q %s -d %s  -e %s  --max-target-seqs 1  -p 64 -a %s"""%( 
+        Type,
         Input,
-        
+
         Database,
-	E_value,
+        E_value,
         OUTPUT 
     )
     os.system(commandline)
-    
+
