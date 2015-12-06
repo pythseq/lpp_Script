bbda3c865e13218a83224a9f6c43c962ac7cd19a
Your Name <lpp1985@hotmail.com>
2015-12-06 23:26:04 +0800

    123

--- COG_mapping.py
+++ COG_mapping.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
 	result_data_frame.to_csv(options.output+".xls",sep="\t",index =False)
 	result_stat_frame = pd.DataFrame( result_data_frame,columns=("COG_FunCat","Category Annotation")  )
 	result_stat_frame = result_stat_frame[ pd.notnull( result_stat_frame["COG_FunCat"]  )   ]
-	result_stat_frame["Gene"] = result_stat_frame["COG_FunCat"]
+
 	result_stat_frame.rename(columns={"COG_FunCat":"Gene"},inplace=True)
 	result_stat_frame = result_stat_frame.groupby(["Category Annotation"] ).agg([ 'count'])
 	TMP2 = tempfile.NamedTemporaryFile()
