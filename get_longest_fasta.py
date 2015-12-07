64392c8e22b7a95ce441793168fd4acbb4d0298b
Your Name <lpp1985@hotmail.com>
2015-12-07 13:19:52 +0800

    123

---
+++ get_longest_fasta.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#coding:utf-8
+"""
+  Author:   -->
+  Purpose: 
+  Created: 2015/12/7
+"""
+from lpp import *
+from optparse import OptionParser
+
+
+
+if __name__ == '__main__':
+	unittest.main()
