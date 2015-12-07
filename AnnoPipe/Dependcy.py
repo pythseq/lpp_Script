89063517b7dc6aa7824a92f98a9cc1e981ab0bcf
Your Name <lpp1985@hotmail.com>
2015-12-07 18:33:35 +0800

    123

--- AnnoPipe/Dependcy.py
+++ AnnoPipe/Dependcy.py
@@ -19,13 +19,14 @@ general_config = ConfigParser()
 general_config.read(
     "general.ini"
 )
-config_hash = Ddict()
+
 def Config_Parse():
 	config_hash = Ddict()
 	for section in general_config.sections():
 		for key in general_config.options(section):
 
 			config_hash[section][key] = general_config.get(section,key)
+			print(general_config.get(section,key))
 	return config_hash
 
 
