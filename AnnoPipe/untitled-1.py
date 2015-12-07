3e466b7ad6b5e08042edf8a936177f97195108fc
Your Name <lpp1985@hotmail.com>
2015-12-07 12:04:02 +0800

    123

--- AnnoPipe/untitled-1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#coding:utf-8
-"""
-  Author:   --<>
-  Purpose: 
-  Created: 2015/12/6
-"""
-from lpp import *
--- get_GO.py
+++ get_GO.py
@@ -5,25 +5,42 @@
   Purpose: 
   Created: 2014/12/30
 """
-
+import sys,shlex,os,subprocess
+from os.path import abspath
+sys.path.append( os.path.split(abspath(__file__))[0]+'/../Lib/' )
 from lpp import *
-GO_Location = "/pub/Database/GO/"
-go_compt = File_dict(open(GO_Location+'/GO_Component','rU')).read(1,2)
-go_def = File_dict(open(GO_Location+'/NAME_DEF.list','rU')).read(1,2)
-id_go = Ddict()
-for line in open(sys.argv[1],'rU'):
-	line_l = line.strip().split("\t")
-	for  e_f in line_l[1:]:
-		id_go[line_l[0]] [e_f] = ""
-END = open(sys.argv[2],'w')
-END.write('GeneID\t'+"GO-BiologicalProcess"+'\t'+"GO-MolecularFunction"+"\tGo-CellularComponent\n")
-end_order = ["biological_process","molecular_function","cellular_component"]
-for key in id_go:
-	all_go = id_go[key]
-	mapp_result = Ddict()
-	for each_go in all_go:
-		mapp_result[go_compt[each_go]  ] [ each_go ]=""
+from Dependcy import *
+from GO_obo_parse import *
 
-	result = "\t".join([ '; '.join([ each_go+'//%s'%(go_def[each_go])  for each_go in   mapp_result[x]  ] )     for x in end_order    ])
-	END.write(key+"\t"+result+'\n')
+usage = '''usage: python2.7 %prog -i input_path -t [The type you want]'''
+parser = OptionParser(usage =usage ) 
+parser.add_option("-i", "--INPUT", action="store", 
+                  dest="input",
+
+                  help="Input File")
+
+
+
+parser.add_option("-o", "--end", action="store", 
+                  dest="output", 
+                  help="OUTPUT Data")
+if __name__ == '__main__':
+	(options, args) = parser.parse_args() 
+	id_go = Ddict()
+	for line in open(options.input,'rU'):
+		line_l = line.strip().split("\t")
+		for  e_f in line_l[1:]:
+			id_go[line_l[0]] [e_f] = ""
+	END = open(options.output,'w')
+	END.write('GeneID\t'+"GO-BiologicalProcess"+'\t'+"GO-MolecularFunction"+"\tGo-CellularComponent\n")
+	end_order = ["biological_process","molecular_function","cellular_component"]
+	for key in id_go:
+		all_go = id_go[key]
+		mapp_result = Ddict()
+		for each_go in all_go:
+			component = GO_COMPONENT.select(GO_COMPONENT.q.Go==each_go)[0].Compent
+			mapp_result[ component ] [ each_go ]=""
+	
+		result = "\t".join([ '; '.join([ each_go+'//%s'%(  GO_DEF.select( GO_DEF.q.Go== each_go )[0].Def   )  for each_go in   mapp_result[x]  ] )     for x in end_order    ])
+		END.write(key+"\t"+result+'\n')
 	
