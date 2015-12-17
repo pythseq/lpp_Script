8ebc226c6592fe7a711857bf415fcb76b65575fe
Your Name <lpp1985@hotmail.com>
2015-12-17 12:37:30 +0800

    123

--- Expression_different_R_static.py
+++ Expression_different_R_static.py
@@ -49,35 +49,35 @@ outputpath   = options.outputpath
 
 para = options.para
 if para not in ["padj","pval"]:
-	raise IOError
+    raise IOError
 
 
 static_path   = os.path.abspath( options.static_path )+os.sep
 
 input_path   = options.input_path
- 
+
 append = options.append
 static_path = options.static_path
 
 if not os.path.exists(  outputpath ):
-	os.makedirs( outputpath )
+    os.makedirs( outputpath )
 
 outputpath = os.path.abspath( outputpath  ) + os.sep
 
 def sampleNameTrans( sample_name ):
-	if re.search( '(^\d+)',sample_name  ):
-		sample_name = 'X'+sample_name
-	return sample_name
+    if re.search( '(^\d+)',sample_name  ):
+        sample_name = 'X'+sample_name
+    return sample_name
 
 if not os.path.exists( static_path  ):
-	print( 'ERROR!!! THE Static PATH doesn\'t exits!!!!!'  )
-	sys.exit()
+    print( 'ERROR!!! THE Static PATH doesn\'t exits!!!!!'  )
+    sys.exit()
 
 if not os.path.exists( input_path  ):
-	print( 'ERROR!!! THE Input expression PATH doesn\'t exits!!!!!'  )
-	sys.exit()
-	
-	
+    print( 'ERROR!!! THE Input expression PATH doesn\'t exits!!!!!'  )
+    sys.exit()
+
+
 
 
 
@@ -87,43 +87,43 @@ input_path = os.path.abspath(  input_path )+os.sep
 size_factor = {}
 
 for each_f in glob.glob(static_path +'*.total.stats'):
-	STATIC = open ( each_f ,'rU' )
-	STATIC.next()
-	sample_name = sampleNameTrans( os.path.split(each_f)[-1].split('.')[0] )
+    STATIC = open ( each_f ,'rU' )
+    STATIC.next()
+    sample_name = sampleNameTrans( os.path.split(each_f)[-1].split('.')[0] )
 
-	size_factor[ sample_name ] = STATIC.next().split('\t')[2]
-	
+    size_factor[ sample_name ] = STATIC.next().split('\t')[2]
+
 for each_matrix in glob.glob(  input_path+'*.'+append  ):
-	stats_name = os.path.split(each_matrix)[-1].split('.')[0]
-	
-	sample_list = [x  for x in stats_name.split('__')]
-	
-	
-	end_path = outputpath+stats_name
-	
-	if not os.path.exists(  end_path ):
-		os.makedirs( end_path )
-	end_path = os.path.abspath(  end_path )+os.sep
-		
-	matrix_output_path   = outputpath + os.path.split(  each_matrix  )[-1]+os.sep+ each_matrix+os.sep
-	MATRIX = open( each_matrix,'rU'  )
-	name_list = MATRIX.next()[:-1].split('\t')
-	x_name = sampleNameTrans( name_list[1] )
-	
-	y_name = sampleNameTrans( name_list[2] )
-	
-	
-	x_coverage = size_factor[ x_name ]
-	
-	y_coverage = size_factor[ y_name  ]
-	
-	matrix_abspath = each_matrix
-	
-	end_prefix = end_path + stats_name
-	
-	
-	
-	r_script = '''#!/usr/bin/Rscript
+    stats_name = os.path.split(each_matrix)[-1].split('.')[0]
+
+    sample_list = [x  for x in stats_name.split('__')]
+
+
+    end_path = outputpath+stats_name
+
+    if not os.path.exists(  end_path ):
+        os.makedirs( end_path )
+    end_path = os.path.abspath(  end_path )+os.sep
+
+    matrix_output_path   = outputpath + os.path.split(  each_matrix  )[-1]+os.sep+ each_matrix+os.sep
+    MATRIX = open( each_matrix,'rU'  )
+    name_list = MATRIX.next()[:-1].split('\t')
+    x_name = sampleNameTrans( name_list[1] )
+
+    y_name = sampleNameTrans( name_list[2] )
+
+
+    x_coverage = size_factor[ x_name ]
+
+    y_coverage = size_factor[ y_name  ]
+
+    matrix_abspath = each_matrix
+
+    end_prefix = end_path + stats_name
+
+
+
+    r_script = '''#!/usr/bin/Rscript
 # functions
 require(ggplot2)
 require(ggthemes)
@@ -178,26 +178,26 @@ ggplot_build(p3)
 dev.off()
 
 '''%(
-	   {
-	   "matrix_abspath":matrix_abspath,
-	   "out_prefix":end_prefix,
-	   "x_name":x_name,
-	   "y_name":y_name,
-	   "x_coverage":x_coverage,
-	   "y_coverage":y_coverage,
-	   "para":para,
-	   "threshold":threshold
-	   
-	   }
-	   
-	   
-	    )
-	RSCRIPT = open( end_path+'Deseq.R'   ,'w' )
-	RSCRIPT.write( r_script )
-	RSCRIPT.close()
-	os.system(  'Rscript ' +RSCRIPT.name +'&')
-	
-	
+       {
+           "matrix_abspath":matrix_abspath,
+           "out_prefix":end_prefix,
+           "x_name":x_name,
+           "y_name":y_name,
+           "x_coverage":x_coverage,
+           "y_coverage":y_coverage,
+           "para":para,
+           "threshold":threshold
+
+       }
+
+
+   )
+    RSCRIPT = open( end_path+'Deseq.R'   ,'w' )
+    RSCRIPT.write( r_script )
+    RSCRIPT.close()
+    os.system(  'Rscript ' +RSCRIPT.name +'&')
+
+
 
 
 
