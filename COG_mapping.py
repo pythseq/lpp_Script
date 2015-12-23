e14e5ad0492889fb8ecea21c70557b385336ba22
Your Name <lpp1985@hotmail.com>
2015-12-23 11:01:15 +0800

    123

--- COG_mapping.py
+++ COG_mapping.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
 		query = line_l[0].split()[0]
 		gene_nog = NOG_GENE.select(AND(NOG_GENE.q.Gene==subj, NOG_GENE.q.NOG.startswith(cog))   )
 		unique = {}
-
+		print( dir( gene_nog ) )
 		if gene_nog.count()>1:
 			gene_nog = gene_nog.first()
 		for each_gene_nog in gene_nog:
