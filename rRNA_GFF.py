87a089a9c6b6249eb79fd0b5a15493411b2a165b
Your Name <lpp1985@hotmail.com>
2016-01-11 15:28:12 +0800

    123

---
+++ rRNA_GFF.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+#coding:utf-8
+"""
+  Author:   ->
+  Purpose: 
+  Created: 2016/1/11
+"""
+from lpp import *
+from collections import namedtuple
+
+all_seq = {}
+
+if __name__ == '__main__':
+	for t,s in fasta_check( open(sys.argv[1],'rU')  ):
+		s1 = re.sub("\s+", "", s)
+		all_seq[ t[1:].split()[0].split("|")[0]   ] = s1
+		
+	RAW = open(sys.argv[2],'rU')
+	GFF  = open( sys.argv[3],'w')
+	SEQ = open(sys.argv[4],'w')
+	for line in RAW:
+		if line.startswith("---"):
+			break
+
+	all_has = {}
+	for line in RAW:
+		data_list =line.strip().split("\t")
+		scaffold = data_list[0]
+		
+		if scaffold in all_has:
+			all_has[scaffold]+=1
+		else:
+			all_has[scaffold]=1
+		data_prefix = "\t".join(data_list[:-1])
+		gene_id = scaffold +'.rRNA.TU.%s'%( all_has[ scaffold ] )
+		mrna_id = scaffold +'.rRNA.%s'%( all_has[ scaffold ] )
+		GFF.write(
+	        "%s\tID=%s;Name=%s\n"%(
+		        data_prefix,
+	            gene_id,
+	            gene_id
+	        )
+	               )
+		
+		GFF.write(
+	        "%s\tID=%s;Parent=%s;Name=%s;product=%s\n"%(
+		        data_prefix,
+	            mrna_id,
+	            gene_id,
+	            mrna_id,
+	            line_l[-1]
+	        )
+	    )			
+		SEQ.write( '>'+mrna_id+'\n'   )
+		exon_num = 0
+		if int(data_list.IntronBegin) ==0:
+			exon_num+=1
+			exon_id = mrna_id+'.exon%s'%(exon_num)
+			GFF.write(
+		        "%s\tID=%s;Parent=%s;Name=%s;product=%s\n"%(
+		            scaffold,
+		            exon_id,
+		            mrna_id,
+		            exon_id,
+		            line_l[-1]
+		        )
+		    )	
+			sequence = all_seq[ scaffold ][ int(line_l[3]):int( line_l[4])   ]
+			if data_list[6]=='-':
+				sequence = complement(sequence)
+			SEQ.write(sequence+'\n')
+			
+				
+	
