5a815abff0d6e21cf3b7756c916787b5376de09c
Your Name <lpp1985@hotmail.com>
2016-01-12 18:54:59 +0800

    123

--- Rfam_scan.py
+++ Rfam_scan.py
@@ -31,67 +31,68 @@ if __name__ == '__main__':
 	e_value = options.evalue
 	outputprefix = options.outputprefix
 	sequence  = options.Sequence
-	command = "cmscan  --noali  --rfam  --acc  --cpu 64 -E %s --tblout  /dev/stdout  %s %s |sort -n -k 8"%( e_value,database,sequence )
-	OUTPUT = os.popen(command)
-
+	cache_name = "/tmp/%s"%(os.getpid())
+	command = "cmscan  --noali  --rfam  --acc  --cpu 64 -E %s --tblout  %s  %s %s |sort -n -k 8"%( e_value,cache_name,database,sequence )
+	os.system(command)
+	OUTPUT = open(  cache_name,'rU'   )
 	genome_hash = {}
 	GFF = open(outputprefix+".gff",'w')
 	CACHE = open("cache",'w')
 	for line in OUTPUT:
 		CACHE.write(line)
-		# if line.startswith("#") or not line.strip():
-			# continue
-		# else:
-			# line_l = line.strip().split()
+		if line.startswith("#") or not line.strip():
+			continue
+		else:
+			line_l = line.strip().split()
 			
-			# if "rRNA" in line_l[0] or "tRNA" in line_l[0]:
-				# continue
-			# source = line_l[2]
-			# product = line_l[0]
-			# if source in genome_hash:
-				# genome_hash[source]+=1
-			# else:
-				# genome_hash[source]=1
-			# gene_ID = source+'.misc_RNA.TU.%s'%(genome_hash[source])
-			# rna_ID = source+'.misc_RNA.%s'%(genome_hash[source])
-			# exon_ID = source+'.misc_RNA.%s.exon1'%(genome_hash[source])
-			# start,end = sorted( [int(line_l[7]),int(line_l[8])] )
-			# frame = line_l[9]
-			# score = line_l[-4]
-			# GFF.write("%s\tInfernal\tgene\t%s\t%s\t%s\t%s\t.\tID=%s;Name=%s\n"%(
-			    # source,
-			    # start,
-			    # end,
-			    # score,
-			    # frame,
-			    # gene_ID,
-			    # gene_ID
-			# )
-			          # )
-			# GFF.write("%s\tInfernal\tmisc_RNA\t%s\t%s\t%s\t%s\t.\tID=%s;Name=%s;Parent=%s;product=%s\n"%(
-				# source,
-				# start,
-				# end,
-				# score,
-				# frame,
-			    # rna_ID,
-			    # rna_ID,
-				# gene_ID,
-			    # product
-			# )
-					  # )		
-			# GFF.write("%s\tInfernal\texon\t%s\t%s\t%s\t%s\t.\tID=%s;Name=%s;Parent=%s;product=%s\n"%(
-			    # source,
-			    # start,
-			    # end,
-			    # score,
-			    # frame,
-			    # exon_ID,
-			    # exon_ID,
-			    # rna_ID,
-			    # product
-			# )
-			          # )				
+			if "rRNA" in line_l[0] or "tRNA" in line_l[0]:
+				continue
+			source = line_l[2]
+			product = line_l[0]
+			if source in genome_hash:
+				genome_hash[source]+=1
+			else:
+				genome_hash[source]=1
+			gene_ID = source+'.misc_RNA.TU.%s'%(genome_hash[source])
+			rna_ID = source+'.misc_RNA.%s'%(genome_hash[source])
+			exon_ID = source+'.misc_RNA.%s.exon1'%(genome_hash[source])
+			start,end = sorted( [int(line_l[7]),int(line_l[8])] )
+			frame = line_l[9]
+			score = line_l[-4]
+			GFF.write("%s\tInfernal\tgene\t%s\t%s\t%s\t%s\t.\tID=%s;Name=%s\n"%(
+			    source,
+			    start,
+			    end,
+			    score,
+			    frame,
+			    gene_ID,
+			    gene_ID
+			)
+			          )
+			GFF.write("%s\tInfernal\tmisc_RNA\t%s\t%s\t%s\t%s\t.\tID=%s;Name=%s;Parent=%s;product=%s\n"%(
+				source,
+				start,
+				end,
+				score,
+				frame,
+			    rna_ID,
+			    rna_ID,
+				gene_ID,
+			    product
+			)
+					  )		
+			GFF.write("%s\tInfernal\texon\t%s\t%s\t%s\t%s\t.\tID=%s;Name=%s;Parent=%s;product=%s\n"%(
+			    source,
+			    start,
+			    end,
+			    score,
+			    frame,
+			    exon_ID,
+			    exon_ID,
+			    rna_ID,
+			    product
+			)
+			          )				
 			
 			
 			
