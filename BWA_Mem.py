c7e62f95ce964a6823957966c765e640387c0356
Your Name <lpp1985@hotmail.com>
2016-01-30 16:58:11 +0800

    123

---
+++ BWA_Mem.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+#coding:utf-8
+# Author:  LPP --<lpp1985@hotmail.com>
+# Purpose: 
+# Created: 2011/7/27
+from lpp import *
+import multiprocessing
+from optparse import OptionParser
+from termcolor import colored
+usage = '''usage: python %prog [options] 
+
+It can automaticly run BWA!!'''
+parser = OptionParser(usage =usage ) 
+
+parser.add_option("-r", "--ref", action="store", 
+                  dest="ref",
+                  type='string',
+                  help="the reference seq")
+
+parser.add_option("-t", "--thread", action="store", 
+                  dest="thread",
+                  type='int',
+                  help="thread number of each BWA")
+
+parser.add_option("-p", "--process", action="store", 
+                  dest="process",
+                  type='int',
+                  help="process number of total BWA")
+
+parser.add_option("-o", "--output", action="store", 
+                  dest="outputpath",
+                  type='string',
+                  help="output path")
+
+parser.add_option("-i", "--input", action="store", 
+                  dest="inputpath",
+                  type='string',
+                  help="input path")
+
+(options, args) = parser.parse_args() 
+ref = options.ref
+thread = options.thread
+
+outputpath = os.path.abspath( options.outputpath)+'/'
+inputpath = os.path.abspath(  options.inputpath )+'/'
+# build index
+os.system( 'bwa index -a is %s 2>&1 >/dev/null'%(  ref  )  )
+def BWA_MAPPING( file_list  ):
+
+	def get_outputname( each_f ):
+		output_preifx = path+name +'.' + re.search( '(pair\d+)$',each_f ).group(1)
+		return output_preifx
+	
+	name = os.path.basename(  file_list[0]   ) .split('.')[0]
+	path = outputpath
+	output_preifx = path+name
+	if not os.path.exists(  path  ):
+		os.makedirs( path )
+	sorted_file = sorted(  file_list,key = lambda x:  int( re.search( 'pair(\d+)'   ,x ) .group(1)    )  )
+	
+
+	[read1_file,read2_file ]= sorted_file
+
+	os.system("bwa mem  -M  -t 64  %s  %s  %s  1> %s.sam  2>/dev/null"%(ref ,read1_file,read2_file,output_preifx ))
+	os.system("samtools view  -bS %s.sam -o %s.bam 2>/dev/null"%( output_preifx, output_preifx ))
+	os.system("samtools sort   %s.bam  %s.sort 2>/dev/null"%( output_preifx, output_preifx ))
+	
+
+output_hash = Ddict()
+for a,b,c in os.walk(  inputpath ):
+	for each_f in c:
+		if re.search('.pair\d+',each_f   ):
+			name = each_f.split('.')[0]
+			output_hash[  name ] [  a+'/'+each_f ] = ''
+input_list = []
+for key in output_hash:
+	input_list.append(  output_hash[key] )
+print( colored(output_hash,'red' ) )
+pool = multiprocessing.Pool(thread)
+pool.map(BWA_MAPPING,input_list)
