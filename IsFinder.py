2d8b6b31d0016faa003402af2da9a58cbbdf19bc
unknown <>
2016-03-10 14:46:38 +0800

    123

--- IsFinder.py
+++ IsFinder.py
@@ -90,7 +90,10 @@ if __name__ == '__main__':
         while not  result:
             time.sleep(5)
             end_output = urllib.urlopen("https://www-is.biotoul.fr/blast/"+out_url).read()
-            print(end_output)
+            if "Query=" in end_output:
+                
+                print(end_output)
+                sys.exit()
             result = re.search("Normal view</a></font><br>(.*)</form>",end_output,re.DOTALL)
         result = result.group(1)
         if result:
