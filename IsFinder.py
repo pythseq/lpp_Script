9132cca2ff8e66666541a7c5f229a01828c71ff1
unknown <>
2016-03-10 17:34:28 +0800

    123

--- IsFinder.py
+++ IsFinder.py
@@ -91,18 +91,14 @@ if __name__ == '__main__':
         while not  result:
             time.sleep(5)
             end_output = urllib.urlopen("https://www-is.biotoul.fr/blast/"+out_url).read()
-            print(  end_output  )
             if "Query=" in end_output:
-                data = end_output.split("</article>")[0]
-                data_list  = data.split("<b>Query=")[1:] 
+                result = end_output.split("</article>")[0]
+                data_list  = result.split("<b>Query=")[1:] 
                 for e_b in data_list:
                     e_b = e_b.replace("</td>","\t</td>").replace("</th></tr>","\n").replace("</th>","\t</th>")
                     data = BeautifulSoup(e_b,"html5lib")
                     
                     print( data.get_text() )                
-                sys.exit()
-            result = re.search("Normal view</a></font><br>(.*)</form>",end_output,re.DOTALL)
-        result = result.group(1)
         if result:
             ALN = open( outputprefix+".xls",'w'  )
             STAT.write("IS_name\tNumber\tAverage.Length\n")
