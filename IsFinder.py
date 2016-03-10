310eb23bf52848277c73278a326aabe702f8bf13
unknown <>
2016-03-10 21:19:04 +0800

    123

--- IsFinder.py
+++ IsFinder.py
@@ -107,6 +107,7 @@ if __name__ == '__main__':
         
                 data = data.get_text() 
                 block_list = data.split("\n\n",2)
+                print(  block_list  )
                 print(block_list[0])
 
             has = {}
