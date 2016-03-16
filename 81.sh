8f3bbe7e91e532d5516f864030de7603f0240360
unknown <>
2016-03-16 23:12:19 +0800

    123

--- 81.sh
+++ 81.sh
@@ -1,6 +1,6 @@
 git add . -A
 git commit -m 123
 #git pull -f Roomnas master:81
-#git pull -f Homenas master:81
-git pull -f USA master:81
+git pull -f Homenas master:81
+#git pull -f USA master:81
 chmod -R 755 ./
