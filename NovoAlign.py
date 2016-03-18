c5bfc070987b8d68fed74c889c45e3fbb3391db5
unknown <>
2016-03-19 00:36:53 +0800

    123

--- NovoAlign.py
+++ NovoAlign.py
@@ -50,11 +50,11 @@ cache_path = outputpath+"/cache/"
 check_path( cache_path )
 
 
-def BWA_MAPPING( file_list  ):
+def MAPPING( file_list  ):
 	
 	name = os.path.basename(  file_list[0]   ) .split('.')[0]
 	path = outputpath
-	data_cache_path = cache_path+"/"+name
+	data_cache_path = cache_path+"/"+name+'/'
 	check_path(data_cache_path)
 	
 	output_preifx = path+name
@@ -71,7 +71,7 @@ def BWA_MAPPING( file_list  ):
 		i+=1
 		index = i%32
 		if index not in out_hash:
-			split_file1 = data_cache_path+'%i/%i.pair1'%( index,index)
+			split_file1 = data_cache_path+'/%i/%i.pair1'%( index,index)
 			check_path(  os.path.dirname( split_file1  ))
 			out_hash[i] = open(split_file1,'w')
 			out_hash[i].write(a+b+c+d)
@@ -81,20 +81,13 @@ def BWA_MAPPING( file_list  ):
 		i+=1
 		index = i%32
 		if index not in out_hash:
-			split_file2 = data_cache_path+'%i/%i.pair2'%( index,index)
+			split_file2 = data_cache_path+'/%i/%i.pair2'%( index,index)
 			check_path(  os.path.dirname( split_file2  ))
 			out_hash[i] = open(split_file2,'w')
 			out_hash[i].write(a+b+c+d)
 			
-	os.system("NovoAligner_Single.py  -r ../%s.ndx  -t 32 -i %s -o "%( index_name,data_cache_path,outputpath+"/"+name    )	
-	
-	#os.system("bwa mem  -M  -t 64  %s  %s  %s  1> %s.sam  2>/dev/null"%(ref ,read1_file,read2_file,output_preifx ))
-	#os.system("samtools view  -@ 20  -bS %s.sam -o %s.bam 2>/dev/null"%( output_preifx, output_preifx ))
-	#os.system("samtools sort  -@ 20  -m 10G  %s.bam    %s.sort 2>/dev/null"%( output_preifx, output_preifx ))
-	os.remove( output_preifx+".bam")
-	os.remove( output_preifx+".sam")
-	shutil.move( output_preifx+'.sort.bam', output_preifx+'.bam')
-	os.system( "samtools index   %s.bam "%(output_preifx))
+	os.system("NovoAligner_Single.py  -r ../%s.ndx  -t 32 -i %s -o %s "%( index_name,data_cache_path,outputpath+"/"+name    ) )	
+	shutil.rmtree(  data_cache_path  )
 	
 
 output_hash = Ddict()
@@ -110,4 +103,4 @@ print( colored(output_hash,'red' ) )
 pool = multiprocessing.Pool(thread)
 
 # map(BWA_MAPPING,input_list)
-pool.map(BWA_MAPPING,input_list)
+pool.map(MAPPING,input_list)
