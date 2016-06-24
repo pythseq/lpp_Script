3886b1572179b8226b958b5e25b5269be40e5e05
unknown <>
2016-06-24 17:01:41 +0800

    123

--- check_overlap_with_junction.py
+++ check_overlap_with_junction.py
@@ -40,7 +40,7 @@ for line in data:
 
 align = sys.stdin
 for  line in align:
-    line_l = re.split("\s+\|*\s*",line[:-1].strip())
+    line_l = re.split("\s+\|*\s*",line[:-1].lstrip())
     q_name = line_l[-2]
     
     q_start,q_end = sorted( [ int(line_l[2]), int(line_l[3])  ] )   
