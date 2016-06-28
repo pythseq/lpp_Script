789de14973869c7835275f4aba61dda292b8534b
unknown <>
2016-06-28 14:00:16 +0800

    123

--- get_complete_Pasa_GFF.py
+++ get_complete_Pasa_GFF.py
@@ -11,7 +11,7 @@ RAW = open(sys.argv[1])
 all_complete = File_dict(open(sys.argv[2],'rU'))
 END = open(sys.argv[3],'w')
 for line in RAW:
-    asid = re.findall( "Target\=(\s+)")
+    asid = re.findall( "Target\=(\s+)",line)
     if asid:
         asid = asid[0]
         if asid in all_complete:
