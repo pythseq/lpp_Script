eb4b24bb97c4adcb59b9bf6656b9008186c0c9bd
unknown <>
2016-06-28 13:58:34 +0800

    123

---
+++ get_complete_Pasa_GFF.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#coding:utf-8
+"""
+  Author:   --<>
+  Purpose: 
+  Created: 2016/6/28
+"""
+
+from lpp import *
+RAW = open(sys.argv[1])
+all_complete = File_dict(open(sys.argv[2],'rU'))
+END = open(sys.argv[3],'w')
+for line in RAW:
+    asid = re.findall( "Target\=(\s+)")
+    if asid:
+        asid = asid[0]
+        if asid in all_complete:
+            END.write(line)
