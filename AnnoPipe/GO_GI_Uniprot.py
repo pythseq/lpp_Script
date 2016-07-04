8fc92029da2b1bca4782a96c60f2f72b8b0f8834
unknown <>
2016-07-04 22:47:41 +0800

    123

--- AnnoPipe/GO_GI_Uniprot.py
+++ AnnoPipe/GO_GI_Uniprot.py
@@ -23,16 +23,16 @@ for line in UNI_GO:
         uniId = unip[ line_l[0] ]
     UNI_GORESULT.write( str(uniId)+'\t'+line_l[-1]+'\n'  )
     
-    for line in UNI_GO:
-        line_l = line.strip().split("\t")
-        gi_list = line_l[-1].split("; ")
-        if line_l[0] not in unip:
-            i+1
-            unip[ line_l[0] ]=i
-            uniId = i
-        else:
-            uniId = unip[ line_l[0] ]
-        for key in gi_list:
-            UNI_GIRESULT.write( str(uniId)+'\t'+key+'\n'  )   
+for line in UNI_GI:
+    line_l = line.strip().split("\t")
+    gi_list = line_l[-1].split("; ")
+    if line_l[0] not in unip:
+        i+1
+        unip[ line_l[0] ]=i
+        uniId = i
+    else:
+        uniId = unip[ line_l[0] ]
+    for key in gi_list:
+        UNI_GIRESULT.write( str(uniId)+'\t'+key+'\n'  )   
 for key,value in unip.items():
     UNIPROT.write( key+'\t'+str(value) +'\n' )
