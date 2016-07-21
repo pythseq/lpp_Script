fc8180fbc664875af77374b310242eeb4546b94e
unknown <>
2016-07-21 16:18:53 +0800

    123

--- Genewise_Parse.py
+++ Genewise_Parse.py
@@ -93,7 +93,7 @@ if __name__ == '__main__':
                 start=start+protein_length+1
                 gff_list[-2]="."
                 gff_list[2] = "nucleotide_to_protein_match"
-                gff_list[5]="%.2f"%(100.0*proteinlength/prot_length)
+                gff_list[5]="%.2f"%(100.0*prot_length/proteinlength)
                 END.write("\t".join(gff_list)+'\n')
                 
         
