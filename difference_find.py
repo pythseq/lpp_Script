336652cc95c2f842e1cc0e0666c3170644df21cc
unknown <>
2016-09-01 21:38:20 +0800

    123

--- difference_find.py
+++ difference_find.py
@@ -35,7 +35,7 @@ print( 'read all' )
 data_blocks = re.split( '\n\n+', all_data)[1:]
 print(  'split all'  )
 name_block = data_blocks[0]
-
+print(name_block)
 align_data = name_block.split('\n')
 
 i=1
@@ -46,7 +46,7 @@ print( 'name ok ' )
 i=1
 reference_name = ''
 for each_block in align_data:
-	name_tag = re.search('^(\w+\s+)', each_block)
+	name_tag = re.search('^(\S+\s+)', each_block)
 	if name_tag:
 		name = name_tag.group(1).strip()
 		aln_b = each_block[name_end:]
@@ -59,6 +59,7 @@ for each_block in align_data:
 		aln_b = each_block[name_end:]
 		detail[ 'align'  ] = aln_b
 	i+=1
+print( detail.keys()  )
 def aln_add( data_string  ):
 	
 	i=1
