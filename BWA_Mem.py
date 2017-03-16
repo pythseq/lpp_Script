5cd9f12887a1453a27791beb9b737044ab11001b
lpp <lpp1985@hotail.com>
2017-03-16 11:00:59 +0800

    123

--- BWA_Mem.py
+++ BWA_Mem.py
@@ -64,7 +64,7 @@ def BWA_MAPPING( file_list  ):
         #print( "bwa mem  -M  -t 64  %s  %s  %s  1> %s.sam  2>/dev/null"%(ref ,read1_file,read2_file,output_preifx ) )
 	os.system("bwa mem  -M  -t 64  %s  %s  %s  1> %s.sam  2>/dev/null"%(ref ,read1_file,read2_file,output_preifx ))
 	os.system("samtools view  -@ 20  -bS %s.sam -o %s.bam 2>/dev/null"%( output_preifx, output_preifx ))
-	os.system("samtools sort  -@ 20    %s.bam    -o %s.sort 2>/dev/null"%( output_preifx, output_preifx ))
+	os.system("samtools sort  -@ 20    %s.bam    -o %s.sort.bam 2>/dev/null"%( output_preifx, output_preifx ))
 	os.remove( output_preifx+".bam")
 	os.remove( output_preifx+".sam")
 	shutil.move( output_preifx+'.sort.bam', output_preifx+'.bam')
