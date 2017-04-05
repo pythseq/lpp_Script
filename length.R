6d316b4b1e674479fc2d4e2012591575c81336f1
lpp <lpp1985@hotail.com>
2017-04-05 20:48:18 +0800

    123

---
+++ length.R
@@ -0,0 +1,12 @@
+#!/usr/bin/Rscript
+library("ggplot2")
+require(ggthemes)
+argv <- commandArgs(TRUE)
+
+countsTable <- read.delim( argv[1], header=TRUE, stringsAsFactors=TRUE )
+pdf(argv[2])
+ggplot(countsTable, aes(Length))+ geom_bar(alpha=0.2)+ylab("Unigene Number")+theme_few()
+dev.off()
+tiff(argv[3])
+ggplot(countsTable, aes(Length))+ geom_bar(alpha=0.2)+ylab("Unigene Number")+theme_few() 
+dev.off()
