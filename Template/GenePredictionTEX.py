b762bfbfab7aa55b8603ca8fc57b8f355af61057
root <root@lpp-ProLiant-MicroServer-Gen8.AC88U>
2017-05-15 17:32:51 +0800

    123

--- Template/GenePredictionTEX.py
+++ Template/GenePredictionTEX.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
 	total_dir = InputPath+"09.AllResult/"
 	os.system(  "cd %s && N50-new.py %s"%(  total_dir, "Total.ffn" )    )	
 	os.system(  "cd %s && lengthN50.R %s"%(  total_dir, "Total.scope  result.pdf result.tiff" )    )	
-	lengh_graph = "%s/result.pdf"%(  total_dir )                                  
+	lengh_graph = "%s/result.tiff"%(  total_dir )                                  
 	anno_path = InputPath+"03.Annotation/"
 	commandline =  """ txt2latex.py -i %s/stats.tsv -o %s/stats.tex   -c  "序列注释结果统计表" """%(  anno_path,anno_path  )  
 
