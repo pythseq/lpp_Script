ad678b70cb0cbd4e46c566548c8bf0d875bdfc39
root <root@lpp-ProLiant-MicroServer-Gen8.AC88U>
2017-05-15 13:43:07 +0800

    123

--- Template/Repeat_Tex.py
+++ Template/Repeat_Tex.py
@@ -48,6 +48,7 @@ if __name__ == '__main__':
 	result_dir = (InputPath+"/02.RepeatMask/")
 	os.system( "enscript -p %s/result.ps %s/*.tbl"%( result_dir,result_dir )   )
 	os.system( "ps2pdf -p %s/result.ps %s/result.pdf"%( result_dir,result_dir )   )
+	print(result_dir)
 	END.write(
 	    template.render(
 	        {
