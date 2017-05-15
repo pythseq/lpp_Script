44c72ace438f380624915741ea545f4515094bfa
root <root@lpp-ProLiant-MicroServer-Gen8.AC88U>
2017-05-15 17:17:43 +0800

    123

--- Template/GenePredictionTEX.py
+++ Template/GenePredictionTEX.py
@@ -54,7 +54,7 @@ if __name__ == '__main__':
 		    each_f,each_f.replace("png","tex"),name
 		) 
 		print(commandline)
-		subprocess.check_call( commandline .split())
+		os.system( commandline )
 		print(   """' Graph2tex.py  -i %s  -o %s -c %s基因组视图  """%(    
 		    each_f,each_f.replace("png","tex"),name
 		)   
