473bbce41a933770f52e79abd175da8923e3c182
root <root@lpp-ProLiant-MicroServer-Gen8.AC88U>
2017-05-15 16:20:27 +0800

    123

---
+++ Template/GenePrediction.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+#coding:utf-8
+"""
+  Author:   --<>
+  Purpose: 
+  Created: 2014/11/12
+"""
+
+
+from lpp import *
+import os
+from  jinja2 import FileSystemLoader,Environment
+from Dependcy import Config_Parse
+from optparse import OptionParser
+
+
+
+
+
+if __name__ == '__main__':
+	usage = '''usage: python2.7 %prog [options] 
+'''
+	parser = OptionParser(usage =usage )
+
+
+
+	parser.add_option("-i", "--InputPath", action="store",
+	                  dest="InputPath",
+
+	                  help="Input Path")
+
+
+	(options, args) = parser.parse_args()
+	InputPath = os.path.abspath(options.InputPath)+'/'
+
+	config_hash = Config_Parse()
+
+	template_root = config_hash["Location"][  "root" ]+"/Template"
+
+
+
+
+	templeloader = FileSystemLoader(template_root)
+	env = Environment(loader = templeloader)
+	template = env.get_template('RepeatMasker.tex')
+	END = open( InputPath+"/RepeatMasker.tex" ,'w' )
+
+	result_dir = InputPath+"/02.RepeatMask/"
+	
---
+++ Template/Graph2tex.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#coding:utf-8
+"""
+  Author:   --<>
+  Purpose: 
+  Created: 2017/5/9
+"""
+import re
+from optparse import OptionParser
+
+	
+if __name__ == "__main__":
+	usage = '''usage: python2.7 %prog [options] 
+'''
+	parser = OptionParser(usage =usage )
+
+	parser.add_option("-i", "--Input", action="store",
+	                  dest="inputData",
+	                  help="Input Data")	
+	parser.add_option("-o", "--Output", action="store",
+	                  dest="Output",
+	                  help=" Output")		
+	parser.add_option("-c", "--Caption", action="store",
+	                  dest="Caption",
+	                  help=" Caption")			
+	(options, args) = parser.parse_args()
+	inputData = options.inputData
+	Output = options.Output
+	Caption = options.Caption
+	OUTPUT = open(  Output,'w' )
+	
+	OUTPUT.write("""
+\\begin{figure}[H]
+
+    \\centering
+    \\includegraphics[width=0.8\\textwidth]{%s}
+    \\captionsetup{labelsep=period}
+    \\caption{%s}
+\\end{figure}
+	
+	
+	"""%(inputData,Caption))
+		
+	
