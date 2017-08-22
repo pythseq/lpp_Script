aaec7fb0dd8d6f51c50cd003ce4d131248adf9e2
lpp <lpp1985@hotail.com>
2017-08-22 12:53:07 +0800

    123

--- pals.sh
+++ pals.sh
@@ -4,7 +4,7 @@ PALS_Align.py $1 pals_hit.gff3
 	piler-64  -trs2fasta trs.gff -seq $1  -path Repeat
 	mkdir Mafft
 
-	for i in Repeat/*.* ; do echo   "mafft  $i > `Mafft/basename $i.mafft`" >>mafft.sh; done
+	for i in Repeat/*.* ; do echo   "mafft  $i > Mafft/`basename $i.mafft`" >>mafft.sh; done
 	cat mafft.sh| parallel -j 64
 	mkdir Cons
 	for i in Mafft/*.mafft; do piler-64 -cons $i -out Cons/`basename ${i%.*}.cons` -label `basename ${i%.*}`;done
