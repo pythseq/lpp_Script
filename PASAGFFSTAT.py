5611df11ff1da3d1ed5f363962b1f3ea3fa4412d
lpp <lpp1985@hotail.com>
2017-08-23 21:46:08 +0800

    123

--- PASAGFFSTAT.py
+++ PASAGFFSTAT.py
@@ -13,7 +13,7 @@ from lpp import *
 if __name__ == '__main__':
 	RAW = open(sys.argv[1], 'rU')
 	data = RAW.read()
-	gene_num = len( set( re.findall("(align_\d+)", data) ) )
+	gene_num = len( set( re.findall("ID\=([^\;]+)", data) ) )
 	
 	RAW.seek(0, 0)
 	r_name = ""
@@ -23,10 +23,14 @@ if __name__ == '__main__':
 	intron_length = 0
 	for line in RAW:
 		line_l = line.split("\t")
-		name = re.search("(align_\d+)", line).group(1)
+		name = re.search("ID\=([^\;]+)", line).group(1)
 		start, end = int(line_l[3]), int( line_l[4]) 
 		if name == r_name:
-			intron_length += start - r_end
+			if start >r_end:
+				intron_length += start - r_end
+				
+			else:
+				intron_length += r_start-end
 			intron_num += 1
 		exon_length += end - start + 1
 		exon_num += 1
@@ -35,4 +39,6 @@ if __name__ == '__main__':
 	length = round( 1.0 * exon_length / exon_num)
 	av_number =  round( 1.0 * exon_num / gene_num)
 	av_intron =   round( 1.0 * intron_length / intron_num)
-	print( "Exon length\t%s\tExon Number\t%s\tAverage Intron\t%s" % ( length, av_number, av_intron) )
+	gen_av = round( 1.0 * exon_length / gene_num )
+	print( "GeneLength\tExon length\tExon Number\tAverage Intron"  )
+	print( "%s\t%s\t%s\t%s" % ( gen_av , length, av_number, av_intron) )
