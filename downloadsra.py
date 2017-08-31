5bd470577a570eea2352dae7dbfb9d4d08339ebc
Your Name <you@example.com>
2017-08-31 23:02:21 +0800

    123

---
+++ downloadsra.py
@@ -0,0 +1,6 @@
+from lpp import *
+all_need = sys.argv[1]
+
+commandline = "ascp -QT -i /home/nfs/SOFTWARE/bin/aspera/etc/asperaweb_id_dsa.openssh  anonftp@ftp-private.ncbi.nlm.nih.gov:/sra/sra-instant/reads/ByRun/sra/%s/%s/%s/%s.sra  . &&  for i in *.sra; do fastq-dump --split-3 `pwd`/$i& done"%( all_need[:3],all_need[:6],all_need,all_need  )
+COM = open("download.sh",'w')
+COM.write(commandline)
