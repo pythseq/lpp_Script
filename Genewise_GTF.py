77ba300173d5fcc2775b621de9327524c7136e2f
Your Name <you@example.com>
2017-09-05 10:51:45 +0800

    123

--- Genewise_GTF.py
+++ Genewise_GTF.py
@@ -25,8 +25,8 @@ if __name__ == '__main__':
 			if line_l[2] == "mRNA":
 				line_l[-2] = "0"
 				end_line = ""
-				trans_id = re.search("ID\=[^\;]+\.(\d+)", line).group(1) + '_t'
-				gene_id = re.search("ID\=[^\;]+\.(\d+)", line).group(1) + '_g'
+				trans_id = re.search("ID\=([^\;]+)", line).group(1) + '_t'
+				gene_id = re.search("Parent\=([^\;]+)", line).group(1) + '_g'
 				new_l =  deepcopy(line_l)
 				if line_l[6] == '+':
 					new_l[2] = "start_codon"
@@ -47,6 +47,7 @@ if __name__ == '__main__':
 					new_l[2] = "start_codon"
 					new_l[3] = str(int(line_l[4])-2)
 					new_l[4] = line_l[4]
+
 					
 					end_line = "\t".join(new_l[:-1]) + '\tgene_id "%s"; transcript_id "%s";\n' % (gene_id, trans_id)
 			if line_l[2] in ["CDS", "exon"]:
