c1c33a45f58a6a397e6efbc0ca9f77b54cba8534
Your Name <you@example.com>
2018-01-02 21:41:48 +0800

    123

--- Quality_Cat.go
+++ Quality_Cat.go
@@ -28,84 +28,82 @@ func main() {
 	GC := 0
 	N := 0
 	totalbase := 0
-	data1IO, _ := lpp.GetBlockRead(*read1, "\n", false, 10000000)
 
 	OUTPUT, err := os.Create(*output + ".stats")
-	data2IO, _ := lpp.GetBlockRead(*read2, "\n", false, 10000000)
+
 	if err != nil {
 		panic("Can not Create Result File!!")
 	}
+	for _, datahandle := range []*string{read1, read2} {
+		for {
+			dataIO, _ := lpp.GetBlockRead(*datahandle, "\n", false, 10000000)
+			line, err := dataIO.Next()
+			if err != nil {
+				break
+			}
+			l += 1
+			if l%4 == 0 {
+				line_cont := bytes.Split(line, []byte("\n"))[0]
+				for i := 0; i < len(line_cont); i++ {
+					if line_cont[i]-qc_number > 20 {
+						Q20 += 1
+						if line_cont[i]-qc_number > 30 {
+							Q30 += 1
+						}
+					}
 
-	for {
+				}
+			} else if l%2 == 0 {
 
-		line, err := data1IO.Next()
-		if err != nil {
-			break
-		}
-		l += 1
-		if l%4 == 0 {
-			line_cont := bytes.Split(line, []byte("\n"))[0]
-			for i := 0; i < len(line_cont); i++ {
-				if line_cont[i]-qc_number > 20 {
-					Q20 += 1
-					if line_cont[i]-qc_number > 30 {
-						Q30 += 1
+				line_cont := bytes.Split(line, []byte("\n"))[0]
+				totalbase += len(line_cont)
+				for i := 0; i < len(line_cont); i++ {
+					if string(line_cont[i]) == "N" {
+						N += 1
+
+					} else if string(line_cont[i]) == "G" || string(line_cont[i]) == "C" {
+
+						GC += 1
 					}
 				}
-
 			}
-		} else if l%2 == 0 {
 
-			line_cont := bytes.Split(line, []byte("\n"))[0]
-			totalbase += len(line_cont)
-			for i := 0; i < len(line_cont); i++ {
-				if string(line_cont[i]) == "N" {
-					N += 1
-					fmt.Println(string(line_cont))
-
-				} else if string(line_cont[i]) == "G" || string(line_cont[i]) == "C" {
-
-					GC += 1
-				}
-			}
 		}
-
 	}
-	for {
+	//	for {
 
-		line, err := data2IO.Next()
-		if err != nil {
-			break
-		}
-		l += 1
-		if l%4 == 0 {
-			line_cont := bytes.Split(line, []byte("\n"))[0]
-			for i := 0; i < len(line_cont); i++ {
-				if line_cont[i]-qc_number > 20 {
-					Q20 += 1
-					if line_cont[i]-qc_number > 30 {
-						Q30 += 1
-					}
-				}
+	//		line, err := data2IO.Next()
+	//		if err != nil {
+	//			break
+	//		}
+	//		l += 1
+	//		if l%4 == 0 {
+	//			line_cont := bytes.Split(line, []byte("\n"))[0]
+	//			for i := 0; i < len(line_cont); i++ {
+	//				if line_cont[i]-qc_number > 20 {
+	//					Q20 += 1
+	//					if line_cont[i]-qc_number > 30 {
+	//						Q30 += 1
+	//					}
+	//				}
 
-			}
-		} else if l%2 == 0 {
+	//			}
+	//		} else if l%2 == 0 {
 
-			line_cont := bytes.Split(line, []byte("\n"))[0]
-			totalbase += len(line_cont)
-			for i := 0; i < len(line_cont); i++ {
-				if string(line_cont[i]) == "N" {
-					N += 1
-					fmt.Println(string(line_cont))
+	//			line_cont := bytes.Split(line, []byte("\n"))[0]
+	//			totalbase += len(line_cont)
+	//			for i := 0; i < len(line_cont); i++ {
+	//				if string(line_cont[i]) == "N" {
+	//					N += 1
 
-				} else if string(line_cont[i]) == "G" || string(line_cont[i]) == "C" {
+	//				} else if string(line_cont[i]) == "G" || string(line_cont[i]) == "C" {
 
-					GC += 1
-				}
-			}
-		}
+	//					GC += 1
+	//				}
+	//			}
+	//		}
 
-	}
+	//	}
 
 	// Result
 	BufOUTPUT := bufio.NewWriterSize(OUTPUT, 9999)
