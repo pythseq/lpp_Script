b2993010130e69615df1d860e67699667d35bd25
lpp1985 <409511038@qq.com>
2018-01-21 23:58:22 +0800

    123

--- pc.sh
+++ pc.sh
@@ -2,7 +2,7 @@ chmod -R 755 ./
 dos2unix *.*
 git add . -A
 git commit -m 123
-#git push -f Homenas pc:master
+git push -f Homenas pc:master
 git push -f Roomnas $1:master
 git push -f USA $1:master
 git push -f GIT $1:master
